7ccb306a7bc87e66038bc5df385786b54ef86d85
11Adars <156577444+11Adars@users.noreply.github.com>
2025-02-09 23:00:16 +0530

    Update background.js

--- Secure Tab 1/background.js
+++ Secure Tab 1/background.js
@@ -2,14 +2,13 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.local.set({ riskySites: [] });
 });
 
-// Listen for tab updates (when a website loads)
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
         checkTab(tab);
     }
 });
 
-// Listen for messages from popup.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "scanTabs") {
         chrome.tabs.query({}, (tabs) => {
@@ -19,7 +18,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 });
 
-// Function to check if a URL is risky
+
 function checkTab(tab) {
     if (!tab.url || tab.url.startsWith("chrome://") ||tab.url.startsWith("https://www.google.com/") || tab.url.startsWith("https://en.wikipedia.org/") || tab.url.startsWith("https://support.microsoft.com/") || tab.url.startsWith("https://www.ibm.com/") || tab.url.startsWith("https://www.youtube.com/") || tab.url.startsWith("https://www.linkedin.com/") || tab.url.startsWith("https://www.chatgpt.com/") || tab.url.startsWith("https://www.flipcart.com/") || tab.url.startsWith("https://www.amazon.com/") || tab.url.startsWith("https://www.whatsapp.com/") || tab.url.startsWith("https://www.facebook.com/") || tab.url.startsWith("https://www.google.com/") || tab.url.startsWith("https://www.mcafee.com/") || tab.url.startsWith("https://www.github.com/") || tab.url.startsWith("https://chrome.google.com/webstore")) {
         return; // Ignore system URLs
@@ -41,7 +40,7 @@ function checkTab(tab) {
     }
 }
 
-// Function to show a warning notification
+
 function showNotification(url) {
     chrome.notifications.create({
         type: "basic",
